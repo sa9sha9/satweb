1271c411f43717f0fadd7a0de669c157452adf05
shunsuke_sakurai <shunsuke_sakurai@MAC10115.local>
2019-04-12 16:05:28 +0900

    Change smooth-scroll interaction

--- app/scripts/interaction.js
+++ app/scripts/interaction.js
@@ -7,5 +7,48 @@
         ;
     });
 
+    $(document).on('click', '.js-smooth-scroll', (e) => {
+        const target = e.target;
+        console.log(target)
+        // clickした要素がclass属性、js-smooth-scrollを含まない場合は処理を中断
+        if (!target.classList.contains("js-smooth-scroll")) return;
+        e.preventDefault();
+        const targetId = target.hash;
+        console.log(targetId)
+        const targetElement = document.querySelector(targetId);
+        // 画面上部から要素までの距離
+        const rectTop = targetElement.getBoundingClientRect().top;
+        // 現在のスクロール距離
+        const offsetTop = window.pageYOffset;
+        // スクロール位置に持たせるバッファ
+        const buffer = 100;
+        const top = rectTop + offsetTop - buffer;
 
+        window.scrollTo({
+            top,
+            behavior: "smooth"
+        });
+    })
+
+    // document.addEventListener("click", e => {
+    //     const target = e.target;
+    //     // clickした要素がclass属性、js-smooth-scrollを含まない場合は処理を中断
+    //     if (!target.classList.contains("js-smooth-scroll")) return;
+    //     e.preventDefault();
+    //     const targetId = target.hash;
+    //     console.log(targetId)
+    //     const targetElement = document.querySelector(targetId);
+    //     // 画面上部から要素までの距離
+    //     const rectTop = targetElement.getBoundingClientRect().top;
+    //     // 現在のスクロール距離
+    //     const offsetTop = window.pageYOffset;
+    //     // スクロール位置に持たせるバッファ
+    //     const buffer = 100;
+    //     const top = rectTop + offsetTop - buffer;
+    //
+    //     window.scrollTo({
+    //         top,
+    //         behavior: "smooth"
+    //     });
+    // });
 }())
